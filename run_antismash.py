fdeaf51587f513764b7337f7a16e59bbee0f6379
victoriapascal <victoria.pascal.andreu@gmail.com>
2020-05-26 14:30:55 +0200

    delete run_antismash.py

--- run_antismash.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Convenience wrapper to run antiSMASH directly from the source tree without installing.
-"""
-
-from antismash.__main__ import entrypoint
-
-if __name__ == '__main__':
-    entrypoint()
